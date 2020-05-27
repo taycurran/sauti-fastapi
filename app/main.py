7ddf67d7b170dc9d7f3e8accda1df026811f52c1
taycurran <taylorcurranagtc@gmail.com>
2020-05-27 13:18:52 -0500

    building app file structure

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
+# to build app
 from fastapi import FastAPI
+# allows web team access to app
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
-import pandas as pd
+# allows you to create a class to structure POST input
 from pydantic import BaseModel
+# allows for html rendering
+from fastapi.responses import HTMLResponse
+# for data handling
+import pandas as pd
+# allows main to get modules from various files in current dir
+# from fastapi import APIRouter
 
+# router = APIRouter()
+
+# ----------------------------------------------------------------------------
+# FastAPI is a Python class that provides all the functionality for this API
+# the app variable serves as an "instance" of the class FastAPI
 app = FastAPI()
 
+# CORS - Cross-Origin Resource Sharing
+# refers to the situations when a frontend running in a browser has 
+# JavaScript code that communicates with a backend, 
+# and the backend is in a different "origin" than the frontend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -14,6 +30,10 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+# "path" is the last part of a URL starting from the first /
+# "path" is commonly called an "endpoint" or "route"
+# "operations" refers to one of the HTTP methods
+# The @ indicates a "path operation decorator"
 @app.get("/")
 async def root():
   """
@@ -25,6 +45,8 @@ async def root():
   <p>Go to <a href="/docs">/docs</a> for documentation.</p>
   """)
 
+
+# POST Root Example
 class Story(BaseModel):
   title: str
   text: str
