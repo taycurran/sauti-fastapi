3ccda3f54bc6eaeef52ea1229f891fe27a5d264e
taycurran <taylorcurranagtc@gmail.com>
2020-05-27 17:13:01 -0500

    Session Maker Established

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
+# To Connect to Labs DB
 from dotenv import load_dotenv
 import os
 import psycopg2
-
 load_dotenv()
 
 conn = psycopg2.connect(dbname=os.getenv("aws_db_name"),
@@ -14,3 +14,17 @@ curs = conn.cursor()
 curs.close()
 conn.close()
 print("Done")
+# ----------------------------------------------------------------------------
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create Connection Engine
+engine = create_engine(os.getenv("aws_db_url"))
+
+# Define Class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# returns a class
+Base = declarative_base()
